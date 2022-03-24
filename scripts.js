f62f71c1267fea9b9bafff5deb65864b45185cd2
trekitch <trekitch@ymail.com>
2022-03-23 21:52:52 -0400

    added starting layout for javascript file

--- scripts.js
+++ scripts.js
@@ -1,1 +1,21 @@
-console.log("testing");
+let myLibrary = [
+    { name: "Hatchet", author: "Gary Paulsen", pages: "195", read: "Yes" },
+    { name: "To Kill a Mockingbird", author: "Harper Lee", pages: "336", read: "No" },
+];
+
+const bookshelf = document.querySelector(".bookshelf");
+
+function Book(name, author, pages, isRead) {
+    this.name = name;
+    this.author = author;
+    this.pages = pages;
+    this.isRead = isRead;
+}
+
+function addBookToLibrary() {}
+
+function displayBooks() {
+    bookshelf.textContent = `Index: ${i}, Name: ${book.name} Author: ${book.author}, Pages: ${book.pages}, Read: ${book.read}`;
+}
+
+displayBooks();
